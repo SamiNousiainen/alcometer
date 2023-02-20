10b56864645939c04c221205e220ff5c53587bea
SamiNousiainen <n1nosa00@students.oamk.fi>
2023-02-20 10:53:09 +0200

    css + render app

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import React from 'react';
 import { useState } from 'react';
-//import styles from './styles/style';
+import { TextInput, TouchableOpacity, Text, View, ScrollView, Switch } from 'react-native';
+import { RadioButton } from 'react-native-paper';
+import NumericInput from 'react-native-numeric-input';
+
+
+import styles from './styles/style';
 
 export default function App() {
 
@@ -13,11 +16,7 @@ export default function App() {
   const [result, setResult] = useState(0);
   const [darkMode, setDarkMode] = useState(false);
 
-  const radioStyle = { flexDirection: 'row', alignItems: 'center' };
-
-  const mode = darkMode ? "Lightmode" : "Darkmode";
-
-  function calc (e) {
+  function calc () {
     const litres = bottles * 0.33;
     const grams = litres * 8 * 4.5;
     const burning = weight / 10;
@@ -42,7 +41,7 @@ export default function App() {
 
   function resultColor (result) {
     if (result == 0) {
-      return {color: 'black'}
+      return {}
     }
     else if (result <= 0.40) {
       return { color: 'green' };
@@ -59,18 +58,47 @@ export default function App() {
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView>
+    <View style={[styles.container, darkMode && styles.nightMode]}>
+      <View style={styles.switch}>
+        <Text style={styles.switchText}>Theme</Text>
+        <Switch
+          value={darkMode}
+          onValueChange={v => setDarkMode(v)} />
+      </View>
+      
+      <Text style={[styles.title, darkMode && styles.nightMode]}>Alcometer</Text>
+
+      <Text style={styles.text}>Insert weight</Text>
+      <TextInput
+        style={styles.textInput}
+        keyboardType='number-pad'
+        value={weight} onChangeText={e => setWeight(e)}
+      />
+
+      <Text style={styles.text}>Bottles drank</Text>
+      <NumericInput style={styles.numericInput} minValue={0} onChange={v => setBottles(v)} />
+
+      <Text style={styles.text}>Time (hours)</Text>
+      <NumericInput minValue={0} style={styles.numericInput} onChange={v => setTime(v)} />
+
+      <RadioButton.Group onValueChange={newValue => setGender(newValue)} value={gender}>
+        <View style={styles.radioButton}>
+          <RadioButton value='male'/>
+          <Text label={'male'} style={styles.text}>Male</Text>
+        </View>
+        <View style={styles.radioButton}>
+          <RadioButton value='female' />
+          <Text label={'female'} style={styles.text}>Female</Text>
+        </View>
+      </RadioButton.Group>
+
+      <TouchableOpacity style={styles.button} onPress={calc}>
+        <Text style={styles.buttonText}>CALCULATE</Text>
+      </TouchableOpacity>
+      <Text style={[styles.result, resultColor(result)]}>{result.toFixed(2)}</Text>
       <StatusBar style="auto" />
+    </View>
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- styles/style.js
+++ styles/style.js
@@ -1,11 +1,72 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
+export default StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        color: '#fff',
+        backgroundColor: '#76b3c2',
+        alignItems: 'center',
+        paddingBottom: 90
     },
-  });
-  
+    title: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 30,
+        padding: 5,
+    },
+    text: {
+        color: '#fff',
+        fontSize: 16,
+        padding: 10,
+    },
+    textInput: {
+        backgroundColor: '#d5eaf0',
+        width: 200,
+        padding: 5,
+        borderRadius: 10,
+        borderWidth: 2,
+        margin: 10,
+        borderColor: '#fff',
+        fontSize: 18,
+    },
+    switch: {
+        marginTop: 20,
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: 5,
+    },
+    switchText: {
+        color: '#fff',
+    },
+    nightMode: {
+        backgroundColor: '#044a5c',
+        color: '#d5eaf0',
+    },
+    button: {
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#fff',
+        height: 50,
+        width: 200,
+        margin: 10,
+        backgroundColor: '#044a5c',
+        
+    },
+    buttonText:{
+        fontSize: 20,
+        color: 'white',
+    },
+    result: {
+        fontSize:30,
+        padding: 10,
+    },
+    radioButton: {
+        flexDirection: 'row', 
+        alignItems: 'center'
+    },
+    numericInput: {
+        width: 100,
+        borderRadius: 10,
+    },
+});
